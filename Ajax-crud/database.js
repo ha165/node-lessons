07a754a4f43ace11104ef6c3fb3ef4df706208c3
ha165 <lumumbaharmony@gmail.com>
2024-07-11 11:18:17 +0300

    Update database.js

---
+++ Ajax-crud/database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+var connection =  mysql.createConnection({
+   host:'localhost',
+   database:'testing',
+   user:'root',
+   password:'@BurneyWailer32'
+});
+
+connection.connect(function(error){
+    if(error)
+    {
+        throw error;
+    }
+    else{
+        console.log('Database Connected Succesfully');
+    }
+});
+
+module.exports = connection;
