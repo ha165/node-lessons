78af6f0827cec0807eaa42621b5b14114b450ddf
ha165 <lumumbaharmony@gmail.com>
2024-07-11 12:59:22 +0300

    Update sample_data.js

--- Ajax-crud/routes/sample_data.js
+++ Ajax-crud/routes/sample_data.js
@@ -1,39 +1,56 @@
+
 var express = require('express');
 
 var router = express.Router();
 
 var database = require('../database');
 
-router.get("/", function (req, res, next) {
-  res.render('sample_data', { title: 'Node JS Ajax CRUD Application' });
+router.get("/", function (request, response, next) {
+
+  response.render('sample_data', { title: 'Node JS Ajax CRUD Application' });
+
 });
-router.post("/action", function (req, res, next) {
-  var action = req.body.action;
+
+router.post("/action", function (request, response, next) {
+
+  var action = request.body.action;
 
   if (action == 'fetch') {
     var query = "SELECT * FROM sample_data ORDER BY id DESC";
 
     database.query(query, function (error, data) {
-      res.json({
+
+      response.json({
         data: data
       });
+
     });
   }
-  if (action == 'add') {
-    var first_name = req.body.first_name;
-    var second_name = req.body.second_name;
-    var age = req.body.age;
-    var gender = req.body.gender;
+
+  if (action == 'Add') {
+    var first_name = request.body.first_name;
+
+    var last_name = request.body.last_name;
+
+    var age = request.body.age;
+
+    var gender = request.body.gender;
 
     var query = `
-     INSERT INTO sample_data(first_name,second_name,age,gender) VALUES ("${first_name}","${second_name}","${age}","${gender}")
-    `;
-    database.query(query,function(error,data){
-      res.json({
-        message : 'Added'
-      })
+		INSERT INTO sample_data 
+		(first_name, last_name, age, gender) 
+		VALUES ("${first_name}", "${last_name}", "${age}", "${gender}")
+		`;
+
+    database.query(query, function (error, data) {
+
+      response.json({
+        message: 'Data Added'
+      });
+
     });
   }
+
 });
 
 module.exports = router;
